daf5252ab0da395470a1a897ec2aaa626d0c86bc
Matt Rinsch <gfunkmonk@mailbox.org>
2026-04-20 22:56:23 -0400

    l

--- scripts/build/arch/parisc.sh
+++ scripts/build/arch/parisc.sh
@@ -3,7 +3,7 @@
 CT_DoArchTupleValues()
 {
     # The architecture part of the tuple:
-    CT_TARGET_ARCH="${CT_ARCH}"
+    CT_TARGET_ARCH="${CT_ARCH}${target_bits_64}"
 }
 
 CT_DoArchUClibcConfig()
@@ -19,22 +19,22 @@ CT_DoArchMultilibTarget ()
     local -a multi_flags=( "$@" )
     local target_
 
-    local m32=false
+    local m31=false
     local m64=false
 
     for m in "${multi_flags[@]}"; do
-        case "$m" in
-            -m32)     m32=true ;;
-            -m64)     m64=true ;;
+        case "${multi_flags}" in
+            -m64) m64=true ;;
+            -m31) m31=true ;;
         esac
     done
 
     eval target_=\"\${${target_var}}\"
 
-    # Fix up bitness
+    # Fix bitness
     case "${target_}" in
-        hppa-*)      $m64 && target_=${target_/#hppa-/hppa64-} ;;
-        hppa64-*)    $m32 && target_=${target_/#hppa64-/hppa-} ;;
+        hppa-*)   $m64 && target_=${target_/#hppa-/hppa64-} ;;
+        hppa64-*)  $m31 && target_=${target_/#hppa64-/hppa-} ;;
     esac
 
     # Set the target variable
